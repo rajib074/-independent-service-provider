327c160fecefee0c1ad27041d17f01fec5268e2f
rajib074 <rajib01711173241@gmail.com>
2022-04-18 00:36:04 +0600

    banar add

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banar from '../Banar/Banar';
 
 const Home = () => {
     return (
         <div>
-          
+          <Banar></Banar>
         </div>
     );
 };
