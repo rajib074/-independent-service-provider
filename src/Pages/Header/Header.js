60c87c005a7974487e5e5abd6cf5c6713f8f213e
rajib074 <rajib01711173241@gmail.com>
2022-04-19 23:44:36 +0600

    header

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -39,44 +39,7 @@ const Header = () => {
         </Container>
       </Navbar>
     </header>
-    // <Navbar sticky="top" className="bg-warning Dark">
-    //   <Container className="d-flex justify-content-around">
-    //     <div>
-    //       <Navbar.Brand as={Link} to="/home">
-    //         <img
-    //           width={100}
-    //           src={
-    //             "https://owly.wpengine.com/wp-content/themes/owly/images/logo.png"
-    //           }
-    //           alt=""
-    //         />
-    //       </Navbar.Brand>
-    //     </div>
-    //     <div>
-    //       <Nav className="Dark">
-    //         <Nav.Link as={Link} to="/">
-    //           Home
-    //         </Nav.Link>
-    //         <Nav.Link as={Link} to="/about">
-    //           About
-    //         </Nav.Link>
-    //         {user && <Nav.Link className='nav-link' as={Link} to='/checkout'>Checkout</Nav.Link>}
-    //         <Nav.Link as={Link} to="/blog">
-    //           Blog
-    //         </Nav.Link>
-    //         {user ? (
-    //           <Nav.Link as={Link} to="login" onClick={handleSignOut}>
-    //           LogOut
-    //         </Nav.Link> 
-    //         ) : (
-    //           <Nav.Link as={Link} to="login">
-    //             Login
-    //           </Nav.Link>
-    //         )}
-    //       </Nav>
-    //     </div>
-    //   </Container>
-    // </Navbar>
+    
   );
 };
 
